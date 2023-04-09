9ba9a238205a9eaa58c10fea5621bf5be8620cb1
Zufaruu <zufarbagas28@gmail.com>
2023-04-09 12:24:26 +0700

    OFDM Tx N64 done

---
+++ Filter/hdlsrc/testing/custom_buffer_compile.do
@@ -0,0 +1,8 @@
+vlib work
+vmap -c
+vcom  custom_buffer_pkg.vhd
+vcom  vector_initialization.vhd
+vcom  vector_feedback.vhd
+vcom  buffer_rsvd.vhd
+vcom  nfp_convert_double_to_fixed_16_En14.vhd
+vcom  custom_buffer.vhd
---
+++ OFDM/Tx_OFDM/clearhighlighting.m
@@ -0,0 +1,14 @@
+SLStudio.Utils.RemoveHighlighting(get_param('Tx_OFDM', 'handle'));
+SLStudio.Utils.RemoveHighlighting(get_param('gm_Tx_OFDM', 'handle'));
+annotate_port('gm_Tx_OFDM/Transmitter/IFFT', 1, 1, '');
+annotate_port('Tx_OFDM/Transmitter/IFFT', 1, 1, '');
+annotate_port('gm_Tx_OFDM/Transmitter/IFFT', 1, 1, '');
+annotate_port('Tx_OFDM/Transmitter/IFFT', 1, 1, '');
+annotate_port('gm_Tx_OFDM/Transmitter/Rectangular QAM Modulator Baseband', 1, 1, '');
+annotate_port('Tx_OFDM/Transmitter/Rectangular QAM Modulator Baseband', 1, 1, '');
+annotate_port('gm_Tx_OFDM/Transmitter/Rectangular QAM Modulator Baseband', 1, 1, '');
+annotate_port('Tx_OFDM/Transmitter/Rectangular QAM Modulator Baseband', 1, 1, '');
+annotate_port('gm_Tx_OFDM/Transmitter/Cosine HDL Optimized/Look-Up Table', 0, 1, '');
+annotate_port('Tx_OFDM/Transmitter/Cosine HDL Optimized/cosine hdl/Look-Up Table', 0, 1, '');
+annotate_port('gm_Tx_OFDM/Transmitter/Sine HDL Optimized/Look-Up Table', 0, 1, '');
+annotate_port('Tx_OFDM/Transmitter/Sine HDL Optimized/sine hdl/Look-Up Table', 0, 1, '');
