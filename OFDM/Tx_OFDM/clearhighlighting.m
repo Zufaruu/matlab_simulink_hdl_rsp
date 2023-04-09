b17862d0869ee8cb537414f53f4c9ca956f9fa5a
Zufaruu <zufarbagas28@gmail.com>
2023-04-09 19:43:30 +0700

    ofdm with ofdm modulator block N_in 64 N_out 128

--- OFDM/Tx_OFDM/clearhighlighting.m
+++ OFDM/Tx_OFDM/clearhighlighting.m
@@ -1,9 +1,7 @@
 SLStudio.Utils.RemoveHighlighting(get_param('Tx_OFDM', 'handle'));
 SLStudio.Utils.RemoveHighlighting(get_param('gm_Tx_OFDM', 'handle'));
-annotate_port('gm_Tx_OFDM/Transmitter/IFFT', 1, 1, '');
-annotate_port('Tx_OFDM/Transmitter/IFFT', 1, 1, '');
-annotate_port('gm_Tx_OFDM/Transmitter/IFFT', 1, 1, '');
-annotate_port('Tx_OFDM/Transmitter/IFFT', 1, 1, '');
+annotate_port('gm_Tx_OFDM/Transmitter/OFDM Modulator1', 1, 1, '');
+annotate_port('Tx_OFDM/Transmitter/OFDM Modulator1', 1, 1, '');
 annotate_port('gm_Tx_OFDM/Transmitter/Rectangular QAM Modulator Baseband', 1, 1, '');
 annotate_port('Tx_OFDM/Transmitter/Rectangular QAM Modulator Baseband', 1, 1, '');
 annotate_port('gm_Tx_OFDM/Transmitter/Rectangular QAM Modulator Baseband', 1, 1, '');
@@ -12,3 +10,6 @@ annotate_port('gm_Tx_OFDM/Transmitter/Cosine HDL Optimized/Look-Up Table', 0, 1,
 annotate_port('Tx_OFDM/Transmitter/Cosine HDL Optimized/cosine hdl/Look-Up Table', 0, 1, '');
 annotate_port('gm_Tx_OFDM/Transmitter/Sine HDL Optimized/Look-Up Table', 0, 1, '');
 annotate_port('Tx_OFDM/Transmitter/Sine HDL Optimized/sine hdl/Look-Up Table', 0, 1, '');
+annotate_port('gm_Tx_OFDM/Transmitter/OFDM Modulator1/OFDM Modulator1', 1, 1, '');
+annotate_port('gm_Tx_OFDM/Transmitter/OFDM Modulator1/OFDM Modulator1', 1, 1, '');
+annotate_port('Tx_OFDM/Transmitter/OFDM Modulator1', 1, 1, '');
