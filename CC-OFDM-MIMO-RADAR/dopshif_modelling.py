b9534109fbfccf6eeb1f098398c5b8dcaa42290d
Zufaruu <zufarbagas28@gmail.com>
2023-09-06 08:21:12 +0700

    cleaning code

--- CC-OFDM-MIMO-RADAR/dopshif_modelling.py
+++ CC-OFDM-MIMO-RADAR/dopshif_modelling.py
@@ -1,22 +1,25 @@
-vel = 20
+vel = 7
 N = 32
+T_symbol = 0.1 * 10e-3
+N_subcarrier = 256
+
 init_lag = 128
-init_distance = 3e8 * (init_lag * 0.1 * 10e-4 /256) / 2
-distance_per_samples = 58.594
+init_distance = 3e8 * (init_lag * T_symbol / N_subcarrier) / 2
+distance_per_samples = 3e8 * (1 * T_symbol / N_subcarrier) / 2
 distance_grid = []
 lag_grid = []
 cur_vel_distance = init_distance
 cur_grid_distance = init_distance
 cur_lag = init_lag
+lag_grid.append(init_lag)
+distance_grid.append(cur_grid_distance)
 
-distance_grid.append(init_distance)
 for i in range(N-1):
     cur_vel_distance += vel
+    distance_grid.append(cur_vel_distance) 
     if(cur_vel_distance < cur_grid_distance + distance_per_samples):
-        distance_grid.append(cur_grid_distance)
         lag_grid.append(cur_lag)
     else:
-        distance_grid.append(cur_grid_distance + distance_per_samples)
         lag_grid.append(cur_lag)
         cur_lag += 1
         cur_grid_distance += distance_per_samples
