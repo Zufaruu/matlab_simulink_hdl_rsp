ad9397afb04b0d103c36f50fe91d0fce631a40d2
Zufaruu <zufarbagas28@gmail.com>
2023-09-27 08:48:39 +0700

    commit file that will be ignored

--- CC-OFDM-MIMO-RADAR/RSPsimulation.m
+++ CC-OFDM-MIMO-RADAR/RSPsimulation.m
@@ -2,24 +2,26 @@ clear;
 %% Params
 n_subcarrier = 256;
 freq_carrier = 3e9;
+freq_IF = 2e7;
+freq_shift = 0;
 lambda = 3e8 / freq_carrier;
-f_start = 20e6;
-f_end = 22.56e6;
 T_symbol = 1e-4;
-symbol_delay = 12;
-freq_sampling = n_subcarrier / T_symbol;
+N_sample = 8192; %8192
+symbols_per_frame = 2;
+pri = T_symbol*symbols_per_frame;
+freq_sampling = N_sample / T_symbol;
 range_per_sampling_period = 3e8 * (1/freq_sampling) / 2;
 rcs = 1;
 Gt = 2936;
 Gr = 2936;
-Bn = (1/T_symbol)/symbol_delay; % BW post doppler
-f = (linspace(f_start, f_end, n_subcarrier))';  %subcarrier frequencies
-t = (linspace(0, T_symbol, n_subcarrier))';     %symbol time
-pri_t = (linspace(0, T_symbol*symbol_delay, n_subcarrier*symbol_delay))'; %pri time
+Bn = (1/T_symbol)/symbols_per_frame;                        % BW post doppler
+t = (linspace(0, T_symbol, n_subcarrier))';                 %symbol time
+t2 = (linspace(0, T_symbol, N_sample))';                    %symbol time interpolated
+pri_t = (linspace(0, pri, N_sample*symbols_per_frame))';    %pri time
 
 %% Wave Generator
 %golay coding
-golay_reg = golay(256);
+golay_reg = golay(n_subcarrier);
 golay_symbol_index = 1;
 golay_code_value = golay_reg(golay_symbol_index, :);
 
@@ -32,17 +34,22 @@ golay_code_value = golay_reg(golay_symbol_index, :);
 %ifft
 iq_signal = ifft(golay_code_value, n_subcarrier)';
 
-% %offset-freq signal
-% freq_shift = 5000
-% iq_signal = frequencyOffset(iq_signal,freq_sampling,freq_shift);
+if N_sample > n_subcarrier
+    a = 1:1:n_subcarrier;
+    b = linspace(1, n_subcarrier, N_sample);
+    iq_signal = interp1(a,iq_signal,b)';
+end
+
+% %offset-freq and IF Freq signal 
+carrier = exp(-1i*2*pi*(freq_IF+freq_shift)*t2);
+iq_signal = iq_signal .* carrier;
 
 %IQ modulator
 i_signal = real(iq_signal);
 q_signal = imag(iq_signal);
 tx_signal = (i_signal + q_signal);
 
-% periodogram(tx_signal, fs)
-% periodogram(tx_signal,[],n_subcarrier,freq_sampling,'centered')
+% periodogram(tx_signal,[],512,freq_sampling,'centered')
 
 % Showing time-domain symbol
 % figure(1);
@@ -50,8 +57,14 @@ tx_signal = (i_signal + q_signal);
 % xlabel('Time (s)');
 % ylabel('Amplitude (V)');
 % title('OFDM Symbol in Time Domain');
+% 
+% figure(9);
+% plot(t2, z);
+% xlabel('Time (s)');
+% ylabel('Amplitude (V)');
+% title('OFDM Symbol Interpolated in Time Domain');
 
-% % showing freq-domain symbol
+% showing freq-domain symbol
 % fft_tx_signal = fft(tx_signal);
 % freq_axis = (0:n_subcarrier-1)*(freq_sampling/n_subcarrier);
 % power = abs(fft_tx_signal).^2/n_subcarrier;
@@ -61,14 +74,34 @@ tx_signal = (i_signal + q_signal);
 % ylabel('Power');
 % title('OFDM Symbol in Frequency Domain');
 
+% fft_z_signal = fft(tx_signal);
+% freq_axis = (0:N_sample-1)*(freq_sampling/N_sample);
+% power = abs(fft_z_signal).^2/N_sample;
+% fft_z_signal_center = fftshift(fft_z_signal);
+% freq_axis_centered = (-N_sample/2:N_sample/2-1)*(freq_sampling/N_sample); 
+% power_centered = abs(fft_z_signal_center).^2/N_sample;
+% 
+% figure(2);
+% plot(freq_axis_centered,power_centered);
+% xlabel('Frequency');
+% ylabel('Power');
+% title('OFDM Symbol in Frequency Domain');
+
+% scope = spectrumAnalyzer(SampleRate=freq_sampling,AveragingMethod="exponential",...
+%     PlotAsTwoSidedSpectrum=false,...
+%     RBWSource="auto",SpectrumUnits="dBW");
+% scope(tx_signal)
+
+% powerbw(tx_signal,freq_sampling)
 %% Channel
 % zero padding and delaying signal
-delay_lag = 128;
-delayed_symbol_signal = circshift(tx_signal, mod(delay_lag, n_subcarrier));
-zero_padded_signal = [tx_signal; zeros((n_subcarrier*(symbol_delay-1)), 1)];
+delay_lag = 500;
+% delayed_symbol_signal = circshift(tx_signal, mod(delay_lag, N_sample));
+ref_signal = [tx_signal; zeros((N_sample*(symbols_per_frame-1)), 1)];
+zero_padded_signal = [tx_signal; zeros((N_sample*(symbols_per_frame-1)), 1)];
 delayed_signal = circshift(zero_padded_signal, delay_lag);
-if delay_lag > 256*11
-    delayed_signal(1:255) = 0;
+if delay_lag > N_sample*(symbols_per_frame-1) && symbols_per_frame ~= 1
+    delayed_signal(1:N_sample-1) = 0;
 end
 
 % attenuating signal (free-space loss in Voltage)
@@ -77,19 +110,25 @@ attenuated_signal = delayed_signal * Lfs / Lfs;
 
 % noising signal
 N0 = 1.38e-23 * 290 * Bn; %temperature noise floor
-corrupted_signal = awgn(attenuated_signal, 40);
+corrupted_signal = attenuated_signal; %awgn(attenuated_signal, 100);
 
 % Showing time-domain channeled signal
-figure(3);
-plot(pri_t, corrupted_signal);
-xlabel('Time (s)');
-ylabel('Amplitude (V)');
-title('Received Signal Across PRI');
+% figure(3);
+% plot(pri_t, corrupted_signal);
+% xlabel('Time (s)');
+% ylabel('Amplitude (V)');
+% title('Received Signal Across PRI');
+
+% figure(10);
+% plot(pri_t, ref_signal);
+% xlabel('Time (s)');
+% ylabel('Amplitude (V)');
+% title('Reference Signal');
 %% RSP
 rx_signal = corrupted_signal;
 
 %cross-correlation
-[c,lags] = xcorr(rx_signal, zero_padded_signal);
+[c,lags] = xcorr(rx_signal, ref_signal);
 
 %range processing
 size_lags =  size(lags, 2);
@@ -100,13 +139,14 @@ c_abs = c(round(size_lags/2):size_lags, 1);
 rxy = c_abs;
 tau = lags_abs * T_symbol / max(lags_abs);
 lag_of_max_c = lags_abs(c_abs == max(c_abs))
-phase_of_max_c = (lag_of_max_c / size(lags_abs, 2)) * 2 * pi;
-
-figure(4);
-stem(tau, rxy);
-xlabel('\tau (s)');
-ylabel('R_x_y');
-title('Cross Correlation of Transmitted and Received Signal');
+% phase_of_max_c = (lag_of_max_c / size(lags_abs, 2)) * 2 * pi;
 
 lag_delay_time = (1/freq_sampling) * lag_of_max_c;
 range_target = 3e8 * lag_delay_time / 2
+
+% figure(4);
+% stem(tau, rxy);
+% xlabel('\tau (s)');
+% ylabel('R_x_y');
+% title('Cross Correlation of Transmitted and Received Signal');
+
--- CC-OFDM-MIMO-RADAR/dopshif_modelling.py
+++ CC-OFDM-MIMO-RADAR/dopshif_modelling.py
@@ -1,11 +1,12 @@
-vel = 7
-N = 32
+vel = 50
+N_observation = 32
 T_symbol = 0.1 * 10e-3
-N_subcarrier = 256
+pri = 0.2e-3
+N_sample = 8192
 
 init_lag = 128
-init_distance = 3e8 * (init_lag * T_symbol / N_subcarrier) / 2
-distance_per_samples = 3e8 * (1 * T_symbol / N_subcarrier) / 2
+init_distance = 3e8 * (init_lag * T_symbol / N_sample) / 2
+distance_per_samples = 3e8 * (1 * T_symbol / N_sample) / 2
 distance_grid = []
 lag_grid = []
 cur_vel_distance = init_distance
@@ -14,9 +15,9 @@ cur_lag = init_lag
 lag_grid.append(init_lag)
 distance_grid.append(cur_grid_distance)
 
-for i in range(N-1):
-    cur_vel_distance += vel
-    distance_grid.append(cur_vel_distance) 
+for i in range(N_observation-1):
+    cur_vel_distance += vel * pri
+    distance_grid.append(round(cur_vel_distance,4)) 
     if(cur_vel_distance < cur_grid_distance + distance_per_samples):
         lag_grid.append(cur_lag)
     else:
